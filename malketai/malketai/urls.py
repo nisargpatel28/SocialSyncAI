2a8a311f70afe0fb9c468e3b8ef3ee196782eb44
Nisarg Patel <patelnisarg.pn@gmail.com>
2026-03-04 01:56:19 -0500

    Added products module

--- malketai/malketai/urls.py
+++ malketai/malketai/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    path('api/products/', include('products.urls')),
 ]
